a57ebc611c3a2849e7a0c3a1600c2de179da6f6f
alejandrou <adhc_1994@hotmail.com>
2018-08-07 00:55:13 +0100

    Creacion metodo XML

--- CRUDxml/Functions.cs
+++ CRUDxml/Functions.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace CRUDxml
 {
     class Functions
     {
+        private XmlDocument BoardXml()//error aqui tambien
+        {
+            
+            XmlDocument doc = new XmlDocument();
+            doc.PreserveWhitespace = true;
+            try { doc.Load("board.xml"); }
+            catch (System.IO.FileNotFoundException)
+            {
+                doc.LoadXml("<?xml version=\"1.0\"?> \n" +
+                "  <!DOCTYPE boardid> \n" + //con SYSTEM "" pero me sale un error extraño
+                "    <board> \n" +
+                "       <post> \n" +
+                "           <id>24.95</id> \n" +
+                "           <username>24.95</username> \n" +
+                "           <text>24.95</text> \n" +
+                "       </post> \n" +
+                "       <post> \n" +
+                "           <id>24.95</id> \n" +
+                "           <username>24.95</username> \n" +
+                "           <text>24.95</text> \n" +
+                "       </post> \n" +
+                "   </board>");
+
+                return doc;
+
+            }
+        }
     }
 }
+
